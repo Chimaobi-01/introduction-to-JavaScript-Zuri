2b29eff5cc9209e723079256abd6e53178a52a01
chimaobi01@gmail.com <chimaobi01@gmail.com>
2022-07-17 07:42:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const name = document.getElementById('name');
+const height = document.getElementById('height');
+const age = document.getElementById('age');
+name.innerHTML = 'Name: Chinaka Chimaobi';
+height.innerHTML = 'Height: 5.6inches';
+age.innerHTML = 'Age: 29';
